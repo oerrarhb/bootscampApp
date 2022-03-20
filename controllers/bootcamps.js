1bd4a15c894cf56099845ad6c4ce6370967cf686
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-03-20 19:47:21 +0100

    add fields selection & sorting

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,11 +7,39 @@ const Bootcamp = require('../models/Bootcamp');
 // @Access Public
 exports.getBootCamps = asyncHandler(async (req, res, next) =>
 {
-        let queryStr = JSON.stringify(req.query);
+        // Extracting query to compute it
+        const reqQuery = {...req.query};
+
+        // Fields to exclude
+        const removeFields = ['select','sort'];
+
+        //Loop over removeFields and delete them from reqQuery
+        removeFields.forEach(param => delete reqQuery[param]);
+
+        let queryStr = JSON.stringify(reqQuery);
         // Follow the doc the extract the comparison
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/,match => `$${match}`);
         let query = Bootcamp.find(JSON.parse(queryStr));
-        bootcamps = await query;
+
+        // select fields
+        if(req.query.select)
+        {
+            const fields = req.query.select.split(',').join(' ');
+            query = query.select(fields);
+        }
+
+        // Sort
+        if(req.query.sort)
+        {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        }
+        else
+        {
+            query = query.sort('-createdAt');
+        }
+
+        const bootcamps = await query;
         res.status(200).json({success : true,count : bootcamps.length, data : bootcamps});
 });
 
