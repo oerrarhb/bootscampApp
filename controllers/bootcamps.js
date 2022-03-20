6573d1d26a7e3294c66b158a786eb072a661f680
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-03-20 20:13:50 +0100

    add pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootCamps = asyncHandler(async (req, res, next) =>
         const reqQuery = {...req.query};
 
         // Fields to exclude
-        const removeFields = ['select','sort'];
+        const removeFields = ['select','sort','page','limit'];
 
         //Loop over removeFields and delete them from reqQuery
         removeFields.forEach(param => delete reqQuery[param]);
@@ -39,8 +39,35 @@ exports.getBootCamps = asyncHandler(async (req, res, next) =>
             query = query.sort('-createdAt');
         }
 
+        //Pagination
+        const page = parseInt(req.query.page,10) || 1;
+        const limit = parseInt(req.query.limit,10) || 25;
+        const startIndex = (page-1)*limit;
+        const endIndex = page*limit;
+        const total = await Bootcamp.countDocuments();
+
+        query = query.skip(startIndex).limit(limit);
+
         const bootcamps = await query;
-        res.status(200).json({success : true,count : bootcamps.length, data : bootcamps});
+
+        const pagination = {};
+        if(endIndex<total)
+        {
+            pagination.next = 
+            {
+                page: page+1,
+                limit
+            }
+        }
+        if(startIndex>0)
+        {
+            pagination.prev = 
+            {
+                page : page -1,
+                limit
+            }
+        }
+        res.status(200).json({success : true,count : bootcamps.length, pagination : pagination,data : bootcamps});
 });
 
 // @Decsription Get bootcamp by id
